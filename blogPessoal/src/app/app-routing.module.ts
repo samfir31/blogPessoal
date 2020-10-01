901b72cd4354a8aaa9fe1d2e86be93d39fe0ee53
unknown <samfir31@gmail.com>
2020-09-30 23:17:33 -0300

    Implementando rotas ao projeto

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { cpuUsage } from 'process';
+import { FeedComponent } from './feed/feed.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'feed', component: FeedComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
