3bd81d37d53dad7a4be31841d8caadf54c6533d9
unknown <samfir31@gmail.com>
2020-10-02 21:48:53 -0300

    Consumindo-api, contruindo componentes login e cadastrar

---
+++ blogPessoal/src/app/service/auth.service.ts
@@ -0,0 +1,20 @@
+import { UserLogin } from './../model/UserLogin';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../model/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  logar(userLogin: UserLogin) {
+    return this.http.post('http://localhost:9000/usuarios/logar', userLogin)
+  }
+
+  cadastrar(user: User) {
+    return this.http.post('http://localhost:9000/usuarios/cadastrar', user)
+  }
+}
